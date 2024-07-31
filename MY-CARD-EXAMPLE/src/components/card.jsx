20fd8049fef345782b4cc39e5319bab479d3874c
User <User@WRSD-LD309-018.dut.ac.za>
2024-07-31 10:16:20 +0200

    changes

---
+++ MY-CARD-EXAMPLE/src/components/card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+
+const Card = () => {
+    return (
+      
+       
+        <>
+        <div class="center-box">
+
+
+        <div class="section1">
+          
+        </div>
+        <div class="section">Section 2</div>
+        <div class="section">Section 3</div>
+        <div class="section">Section 4</div>
+
+
+        </div>
+         </>  
+      
+    );
+  };
+  
+  export default Card;
